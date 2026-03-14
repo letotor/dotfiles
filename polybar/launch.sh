7af3b1c436e59d5082c81734ab7cc8202538bdf5
victorDGW <victor-de-germain_student2022@wilder.school>
2026-03-14 20:53:17 +0100

    Add i3, GTK, rofi and polybar configs
    - i3 config with keybindings, picom, thunar (Catppuccin Mocha theme)
    - GTK 2/3 set to catppuccin-mocha-mauve
    - Rofi Tokyo Night theme
    - Polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Attendre que i3 soit prêt
+sleep 1
+
+# Tuer les instances existantes
+killall -q polybar
+
+# Attendre que polybar se termine
+while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
+
+# Exporter le socket i3
+export I3SOCK=$(i3 --get-socketpath)
+
+# Lancer une barre par moniteur connecté
+if type "xrandr" > /dev/null; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    case $m in
+      eDP-1)   MONITOR=$m polybar -r left &  ;;
+      DP-1-2)  MONITOR=$m polybar -r center & ;;
+      DP-1-3)  MONITOR=$m polybar -r right &  ;;
+    esac
+  done
+fi
